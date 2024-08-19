069ae89d77f254e73002fe5854873a7bf8617d9d
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-19 22:12:14 +0300

    Add project files.

---
+++ ListPractice2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ListPractice2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List <string> coffee = new List <string> (); // Listenin içerisinde string değişkenlerin olduğu boş liste oluşturuyoruz.
+
+            Console.WriteLine("Sevdiğiniz Kahveleri Giriniz.");
+
+            for (int i = 0; i < 5; i++) // 5 kez dönen for döngüsü 
+            {
+                Console.Write($"Kahve {i+1}: "); 
+                string cof = Console.ReadLine() ?? ""; // Kullanıcıdan kahve için string değer alıyoruz
+                coffee.Add(cof); // Aldığımız değerleri listeye ekliyoruz.
+            }
+
+            Console.WriteLine("\nGirilen Kahve İsimleri:");
+
+            foreach (string item  in coffee) // Listedeki elemanları foreach ile ekrana yazdırıyoruz.
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
